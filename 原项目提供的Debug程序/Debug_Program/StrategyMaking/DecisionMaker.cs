7f9f7f0903253fa5b30dad6f2abcf4b4562a6e7c
O-O <529976447@qq.com>
2024-10-29 15:33:18 +0800

    更新原项目提供的Debug功能（配套脚本开发ing）

---
+++ 原项目提供的Debug程序/Debug_Program/StrategyMaking/DecisionMaker.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WSC_SimChallenge_2024_Net.PortSimulation.Entity;
+
+namespace WSC_SimChallenge_2024_Net.PortSimulation
+{
+    public class DecisionMaker
+    {
+        public PortSimModel WSCPort { get; set; }        
+        public Berth CustomeizedAllocatedBerth(Vessel arrivalVessel)
+        {
+            Berth allocatedBerth = null;
+
+            return allocatedBerth;
+        }
+
+        public AGV CustomeizedAllocatedAGVs(Container container)
+        {
+            AGV allocatedAGV = null;
+
+            return allocatedAGV;
+        }
+
+        public YardBlock CustomeizedDetermineYardBlock(AGV agv)
+        {
+            YardBlock yardBlock = null;
+
+            return yardBlock;
+        }
+
+        public DecisionMaker(PortSimModel wSCPort)
+        {
+            WSCPort = wSCPort;
+        }
+
+        public DecisionMaker() { }
+    }
+}
